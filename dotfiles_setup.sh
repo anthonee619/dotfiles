4226e4ccb5d9212af57d74286b5abd1c3dfdb6b9
anthonee619 <anthonee619@gmail.com>
2020-11-10 11:06:49 -0800

    no hidden folders

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-directories=(".bin" ".config/i3" ".config/nvim")
+directories=(".bin" ".config/i3" ".config/nvim" ".config/termite" ".config/polybar")
 for direc in "${directories[@]}"; do
     mkdir ${HOME}/${direc}
 done
 
+
 dotfolder="${HOME}/.dotfiles"
 # ${HOME}
-homeFiles=(".bashrc" ".bash_profile" ".xinitrc")
+homeFiles=(".bashrc" ".bash_profile" ".xinitrc" ".Xresources")
 for dotfile in "${homeFiles[@]}";do
     ln -sf ${dotfolder}/${dotfile} ${HOME}
 done
 
+#lightdm 
+ln -sf ${dotfolder}/lightdm/lightdm.conf /etc/lightdm/lightdm
+
 # ${HOME}/.config/
-configFiles=("nvim/init.vim" "i3/config")
+configFiles=("nvim/init.vim" "i3/config" "termite/config" "polybar/config")
 config="${HOME}/.config"
 for dotfile in "${configFiles[@]}";do
-    ln -sf ${dotfolder}/.config/${dotfile} ${config}/${dotfile} 
+    ln -sf ${dotfolder}/config/${dotfile} ${config}/${dotfile} 
 done
 
 # ${HOME}/.bin/
-for binFilePath in ${HOME}/.dotfiles/.bin/*;do
+for binFilePath in ${HOME}/.dotfiles/bin/*;do
     binefilename="${binFilePath##*/}"
     ln -sf ${binFilePath} $HOME/.bin/$binefilename
 done
